b98c0ad77ffb3aab090eca2f52050593197977d1
Goopil <me@goopil.org>
2026-05-22 09:44:18 +0200

    fix: extends coverage on horizon setup

--- tests/Unit/HorizonOptionalityTest.php
+++ tests/Unit/HorizonOptionalityTest.php
@@ -2,6 +2,7 @@
 
 use Goopil\LaravelRedisSentinel\RedisSentinelManager;
 use Goopil\LaravelRedisSentinel\RedisSentinelServiceProvider;
+use Laravel\Horizon\RedisQueue;
 
 test('core classes do not import Horizon classes directly', function (string $class) {
     $reflection = new ReflectionClass($class);
@@ -19,4 +20,21 @@ test('Horizon context is disabled when Horizon is not configured for Sentinel', 
     $provider = new RedisSentinelServiceProvider(app());
 
     expect($provider->isHorizonContext())->toBeFalse();
+});
+
+test('Horizon queue connector is used when Horizon is configured for Sentinel', function () {
+    config()->set('horizon.driver', 'phpredis-sentinel');
+    config()->set('queue.connections.phpredis-sentinel', [
+        'driver' => 'phpredis-sentinel',
+        'connection' => 'phpredis-sentinel',
+        'queue' => 'default',
+    ]);
+
+    $provider = new RedisSentinelServiceProvider(app());
+    $method = new ReflectionMethod($provider, 'bootQueue');
+    $method->setAccessible(true);
+    $method->invoke($provider);
+
+    expect($provider->isHorizonContext())->toBeTrue()
+        ->and(app()->make('queue')->connection('phpredis-sentinel'))->toBeInstanceOf(RedisQueue::class);
 });
