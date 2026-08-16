015f055bf4c3e691289e62030bfecdb277f58283
Goopil <me@goopil.org>
2026-08-16 11:39:35 +0200

    fix: add missing SimulatedJobFailure imports in workbench Jobs

--- workbench/app/Jobs/BatchableJob.php
+++ workbench/app/Jobs/BatchableJob.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
+use Workbench\App\Exceptions\SimulatedJobFailure;
 
 class BatchableJob implements ShouldQueue
 {
--- workbench/app/Jobs/SendEmailJob.php
+++ workbench/app/Jobs/SendEmailJob.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
+use Workbench\App\Exceptions\SimulatedJobFailure;
 
 class SendEmailJob implements ShouldQueue
 {
